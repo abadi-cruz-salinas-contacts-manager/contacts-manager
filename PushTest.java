43aec45b14324b900b1af6b70633ab0839597467
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-10 15:02:18 -0600

    new start

--- PushTest.java
+++ PushTest.java
@@ -1,5 +1,5 @@
 public class PushTest {
-//    public static void main(String[] args) {
-//        System.out.println("this is a test");
-//    }
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
 }
