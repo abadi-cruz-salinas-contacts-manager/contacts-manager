b2f87c83017701103c31e671368dbb5819693d04
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-11 14:34:13 -0600

    Did it

--- contactManager/Contact.java
+++ contactManager/Contact.java
@@ -1,28 +1,16 @@
 package contactManager;
 
-public class Contact {
-        public String name;
-        public int number;
-        // constructor
-        public Contact(String name, int phoneNumber) {
-            this.name = name;
-            this.number = phoneNumber;
-        }
-
-        public String getName(){
-            return this.name;
-        }
-
-        public void setName(String name){
-            this.name = name;
-        }
-
-        public int getNumber(){
-            return this.number;
-        }
-
-        public void setNumber(int number){
-            this.name = name;
-        }
+class Contact{
+    String name;
+    String phone;
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
     }
-
+    public String getName() {
+        return this.name;
+    }
+    public String getPhone() {
+        return this.phone;
+    }
+}
